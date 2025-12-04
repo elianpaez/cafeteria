c9599a7ab1c5ae66c01a283fb6a83998319446bc
elianpaez <elianpaez72@gmail.com>
2025-12-04 13:12:31 -0300

    env creado

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCltcV_F6OIFbpnzp-H3B4vrpoloWvy2WI",
-  authDomain: "cafeteria-3f60c.firebaseapp.com",
-  projectId: "cafeteria-3f60c",
-  storageBucket: "cafeteria-3f60c.firebasestorage.app",
-  messagingSenderId: "180454999114",
-  appId: "1:180454999114:web:0458399eaf946fc49852b3",
-  measurementId: "G-KPJ7DNQ4HH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
